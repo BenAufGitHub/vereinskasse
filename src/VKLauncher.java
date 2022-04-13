2dcafd77c7e8ea06d00857105cc9da196b089b26
Ben Mette <benme04@gmail.com>
2022-04-13 18:27:52 +0200

    Init Projekt Struktur

---
+++ src/VKLauncher.java
@@ -0,0 +1,11 @@
+/**
+ * Der Vereinskassen-Launcher umfasst den Startpunkt des Programmes.
+ * Nur main-Methode benutzen! (oder statische Methoden)
+ */
+public class VKLauncher {
+
+    public static void main(String[] args) {
+        // TODO
+    }
+
+}
