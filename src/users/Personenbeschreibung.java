666739ff712aa92af21b95ad6a3d91ff01e66b98
Ben Mette <benme04@gmail.com>
2022-04-17 22:04:22 +0200

    Vergleiche von Profilen mit Comparable Interface

--- src/users/Personenbeschreibung.java
+++ src/users/Personenbeschreibung.java
@@ -1,7 +1,7 @@
 package users;
 
 /** Zum Listen der Personen mit wenig Speicher-Gebrauch */
-public class Personenbeschreibung {
+public class Personenbeschreibung implements Comparable<Personenbeschreibung>{
     public final String vorname;
     public final String nachname;
     public final int id;
@@ -10,5 +10,14 @@ public class Personenbeschreibung {
         this.vorname = vorname;
         this.nachname = nachname;
         this.id = id;
+    }
+
+    @Override
+    public int compareTo(Personenbeschreibung o) {
+        if(this.vorname.compareTo(o.vorname) == 1) return 1;
+        if(this.vorname.compareTo(o.vorname) == -1) return -1;
+        if(this.nachname.compareTo(o.nachname) == 1) return 1;
+        if(this.nachname.compareTo(o.nachname) == -1) return -1;
+        return (this.id < o.id) ? -1 : 1;
     }
 }
