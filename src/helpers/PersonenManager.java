af6db8d8117e3a1aa7933826f34cc4a850b596c9
Ben Mette <benme04@gmail.com>
2022-04-18 22:28:31 +0200

    PersonenManager fertig

--- src/helpers/PersonenManager.java
+++ src/helpers/PersonenManager.java
@@ -3,14 +3,25 @@ package helpers;
 import users.Person;
 import users.Personenbeschreibung;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class PersonenManager {
 
     private Profilliste alle;
     private Profilliste negative;
+    private Metadata data;
+    private static String metafile = "metadata.json";
 
 
     public PersonenManager(){
-        // TODO anlegen einer config klasse und config file.
+        this.data = SaveAssistant.ladeObjekt(metafile, Metadata.class);
+        // kein vorhandenes data file
+        if(this.data == null)
+            this.data = createDataSet();
+        ArrayList<Personenbeschreibung> pbs = SaveAssistant.getPersonenbeschreibungen();
+        initListen(pbs);
     }
 
 
@@ -24,12 +35,19 @@ public class PersonenManager {
      * @param urspruenglich Das Profil der Person, welches vor den Veränderungen angelegt war.
      */
     public void save(Person person, Personenbeschreibung urspruenglich) {
-        // TODO
+        if(person == null && urspruenglich == null) return;
+        if(person == null)
+            removePerson(urspruenglich);
+        else if(urspruenglich == null)
+            createPerson(person);
+        else
+            changePerson(person, urspruenglich);
     }
 
     public int getNewID() {
-        // TODO
-        return 0;
+        int id = ++(data.vergebeneIDs);
+        data.save();
+        return id;
     }
 
     public Profilliste getAlleProfile(){
@@ -38,6 +56,96 @@ public class PersonenManager {
 
     public Profilliste getSchuldhafteProfile() {
         return negative;
+    }
+
+
+    // ============================== Private Methoden ============================
+
+
+    private void removePerson(Personenbeschreibung pb) {
+        alle.delete(pb);
+        negative.delete(pb);
+        data.schuldhafteIDs.remove(pb.id);
+        data.totalUsers--;
+        SaveAssistant.loeschePerson(pb);
+        data.save();
+
+    }
+
+    private void createPerson(Person person) {
+        SaveAssistant.speicherePerson(person);
+        alle.insert(person.getBeschreibung());
+        data.totalUsers++;
+    }
+
+    private void changePerson(Person person, Personenbeschreibung zuletztPB) {
+        Personenbeschreibung aktuell = aktualisiereBeschreibung(person, zuletztPB);
+        SaveAssistant.speicherePerson(person);
+        if(person.istSchuldenfrei())
+            data.schuldhafteIDs.remove(aktuell.id);
+        else
+            data.schuldhafteIDs.add(aktuell.id);
+    }
+
+    private Personenbeschreibung aktualisiereBeschreibung(Person person, Personenbeschreibung zuletztPB) {
+        Personenbeschreibung neu = person.getBeschreibung();
+        if(neu.compareTo(zuletztPB) == 0) return zuletztPB;
+        alle.delete(zuletztPB);
+        negative.delete(zuletztPB);
+        SaveAssistant.bennenePersonUm(neu, zuletztPB);
+        return neu;
+    }
+
+    private Metadata createDataSet() {
+        data = new Metadata();
+        data.evalEverything();
+        data.save();
+        return data;
+    }
+
+    private void initListen(ArrayList<Personenbeschreibung> pbs) {
+        this.alle = new Profilliste(pbs);
+        pbs.removeIf((element) -> {
+            return data.schuldhafteIDs.contains(element.id);
+        });
+        this.negative = new Profilliste(pbs);
+    }
+
+
+    // ============================ Metadata ======================================
+
+
+    private class Metadata {
+
+        private HashSet<Integer> schuldhafteIDs;
+        private int vergebeneIDs;
+        private int totalUsers;
+
+        public Metadata() {
+            this.schuldhafteIDs = new HashSet<>();
+            this.vergebeneIDs = 0;
+            this.totalUsers = 0;
+        }
+
+
+        /** Scans all data, should not be used frequently. */
+        private void evalEverything() {
+            List<Personenbeschreibung> personen = SaveAssistant.getPersonenbeschreibungen();
+            this.totalUsers = personen.size();
+            int maxID = 0;
+            for(Personenbeschreibung pb : personen) {
+                int schulden = SaveAssistant.greifeSchuldenBetrag(pb);
+                maxID = Math.max(maxID, pb.id);
+                if(schulden <= 0) continue;
+                schuldhafteIDs.add(pb.id);
+            }
+            vergebeneIDs = maxID;
+        }
+
+        public void save() {
+            SaveAssistant.speichereObjekt(this, metafile);
+        }
+
     }
 
 }
