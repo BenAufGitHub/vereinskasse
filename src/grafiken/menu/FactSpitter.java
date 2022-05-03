8b9ccd63822d99c8bae212477a382b2c11b147cd
Ben Mette <benme04@gmail.com>
2022-05-03 16:33:13 +0200

    Bonus-Fakten

--- src/grafiken/menu/FactSpitter.java
+++ src/grafiken/menu/FactSpitter.java
@@ -17,7 +17,17 @@ public class FactSpitter {
             "Die größte ethnische Gruppe in Bayern sind die Deutschen",
             "Insekten sind gegen Fallschaden immun, da sieht keine großen Geschwindigkeiten und Aufprallkräfte erreichen.",
             "Denk dran die Mülltonne rauszustellen.",
-            "Fast daneben ist auch vorbei"
+            "Fast daneben ist auch vorbei",
+            "\"Every 60 seconds, a minute passes\"",
+            "Die Wahrscheinlichkeit, in einem Haus mit ungerader Hausnummer zu leben, liegt bei circa 50,2%.",
+            "\"Nein\" ~Johann Wolfang von Goethe, Faust I: V.63",
+            "Anhänger der Shopping-Card-Theory besagen, man könne den Charakter eines Menschen durch die Frage bestimmen, " +
+                    "ob dieser seinen Einkaufwagen zurückstellen würde, wenn keiner hinschauen würde.",
+            "Mischt man ein Blackjack-Deck, kann man sich sicher sein, dass die resultierende Kartenfolge in der Geschichte noch nie vorgekommen ist.",
+            "Clinophobie bezeichnet die Angst, ins Bett zu gehen.",
+            "Etwa 99% aller Kurven sind Rechtskurven.",
+            "Viele Star Wars Fans sind sich einig: Jar-Jar Binks ist ein Sith-Lord.",
+            "Verlasse das Gebäude nicht, es sei denn, du willst raus!"
     };
 
     public String spit() {
