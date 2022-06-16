8f8dcab06a7de226069915af4fbb723ed214f12d
Ben Mette <benme04@gmail.com>
2022-06-17 00:19:30 +0200

    Fix: Negative Schulden

--- src/grafiken/bearbeiten/AddPopUp.java
+++ src/grafiken/bearbeiten/AddPopUp.java
@@ -50,7 +50,7 @@ public class AddPopUp extends JDialog {
     private void addButton(JPanel p, JTextField grund, JTextField betrag, JButton add) {
         add.addActionListener((e) -> {
             String amount = betrag.getText().strip();
-            if(!GeldFormat.isValidMoney(amount)) {
+            if(!GeldFormat.isValidMoney(amount) || GeldFormat.toGeld(amount)<=0.0) {
                 betrag.setBackground(Color.RED);
                 return;
             }
